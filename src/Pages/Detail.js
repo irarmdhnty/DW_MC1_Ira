14de355908b5b7b3ab4d1808b8fbefd6c0be0782
irarmdhnty <irarmdhnty03@gmail.com>
2022-10-21 14:48:16 +0700

    update

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -4,33 +4,31 @@ import { Data } from "../DataDummy/Data";
 
 const Detail = () => {
     const id = useParams();
-    const profile = Data.filter((p) => p.id)
+    const list = Data.filter((p) => p.id == id.id)
   return (
     <Container>
-      {Data.map((item, index) => (
-        <div key={index} className="my-3 col-md-6">
+        <div className="my-3 col-md-6">
           <Card.Body className="p-2 shadow" style={{ textAlign: "center" }}>
             <img
               variant="top"
-              src={item.image}
+              src={list[0].image}
               style={{ width: "50px", marginRight: "15px" }}
             />
 
-            <p>@{item.username}</p>
+            <p>@{list[0].username}</p>
             <Row>
               <Col>
-                <p>{item.follower}</p>
+                <p>{list[0].follower}</p>
                 <p>Follower</p>
               </Col>
               <Col>
-                <p>{item.following}</p>
+                <p>{list[0].following}</p>
                 <p>Following</p>
               </Col>
             </Row>
             <Link to="/" >back</Link>
           </Card.Body>
         </div>
-      ))}
     </Container>
   );
 };
